f33d6f817dbc82eac0440783eb15f0be0bb7be70
Brian Greene <brian.shadeofgreene@gmail.com>
2017-10-28 12:25:08 -0600

    Commit

--- src/index.js
+++ src/index.js
@@ -2,4 +2,14 @@
   Add your logic here to initialize a service of your choice
  */
 
+ import express from 'express'
+
+ const app = express()
+
+ app.get('/', function (req, res) {
+  res.send('hello world')
+})
+
+app.listen(3000)
+
 export default true
